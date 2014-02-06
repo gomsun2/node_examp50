bf92504616799b35429b7f78474dd2b6afb29499
jhjang.macbookPro <gomsun2@gmail.com>
2014-02-06 21:51:40 +0900

    작동안하는 문제 수

--- leanNodeBy50Examp/chapter_01/5.StreamingPerformanceOptimize/server.js
+++ leanNodeBy50Examp/chapter_01/5.StreamingPerformanceOptimize/server.js
@@ -1,7 +1,3 @@
-/**
- * Created by mac on 2014. 2. 5..
- */
-
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
@@ -11,34 +7,38 @@ var mineTypes = {
   '.html':'text/html',
   '.css':'text/css'
 };
-var cache = {};
 
+var cache = {};
 http.createServer(function(request, response){
-  console.log('createServer');
   var lookup = path.basename(decodeURI(request.url)) || 'index.html'
-    , filename = 'content/' + lookup;
+    ,filename = 'content/' + lookup;
   fs.exists(filename, function(exists){
-    var headers = {'Content-Type':mineTypes[path.extname(filename)]};
-    if(cache[filename]){
-      response.writeHead(200, headers);
-      response.end(cache[filename]);
+    if(exists){
+      var headers = {'Content-Type':mineTypes[path.extname(filename)]};
+      if(cache[filename]){
+        console.log('from cache');
+        response.writeHead(200, headers);
+        response.end(cache[filename].content);
+        return;
+      }
+      var stream = fs.createReadStream(filename).once('open', function(){
+        response.writeHead(200, headers);
+        this.pipe(response);
+      }).once('error', function(e){
+        console.log('fs.createReadStream error:' + e);
+        response.writeHead(500);
+        response.end('internal server error!!');
+      });
+      fs.stat(filename, function(err, stats){
+        var bufferOffset = 0;
+        cache[filename] = {content: new Buffer(stats.size)};
+        stream.on('data', function(chuck){
+          chuck.copy(cache[filename].content, bufferOffset);
+          bufferOffset += chuck.length;
+        });
+      });
       return;
     }
-    var s = fs.createReadStream(filename).once('open', function(){
-      response.writeHead(200, headers);
-      this.pipe(response);
-    }).once('error', function(e){
-      console.log('fs.createReadStream error:' + e);
-      response.writeHead(500);
-      response.end('internal server error!!');
-    });
-    fs.stat(filename, function(err, stats){
-      var bufferOffset = 0;
-      cache[filename] = {content: new Buffer(stats.size)};
-      s.on('data', function(chuck){
-        chuck.copy(cache[filename].content, bufferOffset, bufferOffset += chuck.length);
-      });
-    });
     response.writeHead(404);
     response.end('page not found!');
   });
